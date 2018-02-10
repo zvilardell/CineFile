76513ec726d1f28e16f75579de07cfe4394ae61d
Zach Vilardell <szvila01@louisville.edu>
2018-02-09 21:39:13 -0500

    new project

---
+++ CineFile/CineFile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CineFile
+//
+//  Created by Zach Vilardell on 2/9/18.
+//  Copyright © 2018 zvilardell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
