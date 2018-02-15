a382f475c3f9b0e3d0508a796809ab0a39e2d904
Zach Vilardell <szvila01@louisville.edu>
2018-02-15 17:05:38 -0500

    building out api service

--- CineFile/CineFile/APIRequestManager.swift
+++ CineFile/CineFile/APIRequestManager.swift
@@ -14,6 +14,7 @@ class APIRequestManager: NSObject {
     var tmdbKey: String!
     var baseRequestURL = "https://api.themoviedb.org/3"
     var configurationEndpoint = "/configuration"
+    var searchEndpoint = "/search/movie"
     
     //---------------------------------------------------------------------------------------------------------------------------
     //singleton setup
@@ -26,6 +27,8 @@ class APIRequestManager: NSObject {
         let plistDict = NSDictionary(contentsOfFile: plistPath),
         let tmdbKey = plistDict["TMDb API Key"] as? String {
             self.tmdbKey = tmdbKey
+            //make initial api call for configuration data
+            getConfiguration()
         } else {
             print("Unable to retrieve API key.")
         }
@@ -40,9 +43,37 @@ class APIRequestManager: NSObject {
         ]
         Alamofire.request(baseRequestURL + configurationEndpoint, method: .get, parameters: parameterDict).responseJSON { response in
             if let responseDict = response.result.value as? NSDictionary, let imageConfigDict = responseDict["images"] as? NSDictionary {
-                print(responseDict)
+                //print(responseDict)
                 print(imageConfigDict["secure_base_url"] as! String)
-                print(imageConfigDict["poster_sizes"] as! [String])
+                let sizes = imageConfigDict["poster_sizes"] as! [String]
+                let filteredSizes = sizes.filter {string in
+                    return string.count == 4 && string >= "w500"
+                }
+                print(filteredSizes)
+            }
+        }
+    }
+    
+    func searchMoviesByTitle(title: String) {
+        let parameterDict: [String:String] = [
+            "api_key" : tmdbKey,
+            "query"   : title
+        ]
+        Alamofire.request(baseRequestURL + searchEndpoint, method: .get, parameters: parameterDict).responseJSON { response in
+            if let responseDict = response.result.value as? NSDictionary, let searchResults = responseDict["results"] as? [NSDictionary] {
+                print(searchResults[0])
+            }
+        }
+    }
+    
+    func getMovieByID(id: UInt) {
+        let parameterDict: [String:String] = [
+            "api_key"  : tmdbKey
+        ]
+        let movieEndpoint = "/movie/\(id)"
+        Alamofire.request(baseRequestURL + movieEndpoint, method: .get, parameters: parameterDict).responseJSON { response in
+            if let responseDict = response.result.value as? NSDictionary {
+                print(responseDict)
             }
         }
     }
--- CineFile/CineFile/ViewController.swift
+++ CineFile/CineFile/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        APIRequestManager.sharedInstance.getConfiguration()
+        APIRequestManager.sharedInstance.searchMoviesByTitle(title: "Superbad")
+        APIRequestManager.sharedInstance.getMovieByID(id: 8363)
     }
 
     override func didReceiveMemoryWarning() {
