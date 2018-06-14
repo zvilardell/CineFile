6c0834e64d91abf692d97c49c00f357d6de9f451
Zach Vilardell <szvila01@louisville.edu>
2018-06-13 21:08:40 -0700

    initial barcode scanning setup complete

--- CineFile/CineFile/ViewControllers/MovieSearch/BarcodeScanning/BarcodeScanViewController.swift
+++ CineFile/CineFile/ViewControllers/MovieSearch/BarcodeScanning/BarcodeScanViewController.swift
@@ -7,29 +7,24 @@
 //
 
 import UIKit
+import MTBBarcodeScanner
 
 class BarcodeScanViewController: UIViewController {
 
+    var barcodeScanner: MTBBarcodeScanner?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        barcodeScanner = MTBBarcodeScanner(previewView: view)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        barcodeScanner?.stopScanning()
+        super.viewWillDisappear(animated)
+    }
 
 }
--- CineFile/CineFile/ViewControllers/MovieSearch/MovieSearchViewController.swift
+++ CineFile/CineFile/ViewControllers/MovieSearch/MovieSearchViewController.swift
@@ -30,17 +30,9 @@ class MovieSearchViewController: UIViewController {
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func scanbarCode(_ sender: UIBarButtonItem) {
+        present(BarcodeScanViewController(), animated: true, completion: nil)
     }
-    */
-
 }
 
 //MARK: - UICollectionViewDataSource
